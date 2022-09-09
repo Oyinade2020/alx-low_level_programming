07ef6219ddde733709c790cfdd908a400220e3b6
Oyinlola Adesokan <sockistical@gmail.com>
2022-09-09 11:07:44 -0700

    zero, +ve, -ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes here*/
-	if(n>0)
+	if (n > 0)
 	{
 		printf("%i is positive\n",n);
-	}else if(n==0)
+	}else if (n == 0)
 	{
 		printf("%i is zero\n", n);
 	}
