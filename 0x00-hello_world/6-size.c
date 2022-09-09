9a53f169c0519578ed6eb7c1e814517a438611bd
Oyinlola Adesokan <sockistical@gmail.com>
2022-09-08 17:15:10 -0700

    last exercise

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	printf("Size of a char: %1d bytes\n", sizeof(c));
+	printf("Size of an int: %1d bytes\n", sizeof(i));
+	printf("Size of a long: %1d bytes\n", sizeof(li));
+	printf("Size of a long long int: %1d bytes", sizeof(lli));
+	printf("Size of a float: %1d bytes", sizeof(f));
+	return (0);
+}
