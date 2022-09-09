eb04dd2299402d38ee945e66f0f8c4aca7d3bbf4
Oyinlola Adesokan <sockistical@gmail.com>
2022-09-09 16:20:40 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always return 0 (success)
+ *
+ * Description: prints lowercase in reverse
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
