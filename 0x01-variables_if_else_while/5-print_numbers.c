f3abe3617c051055c34dcae3e3f71b3879cc3efc
Oyinlola Adesokan <sockistical@gmail.com>
2022-09-09 15:48:52 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ * Description: prints single digit numbers
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n<= '9'; n++)
+	{
+	putchar(n)
+	}
+	putchar('\n');
+	return (0);
+}	
