c511d514d480472269a25702f808e7d67534392b
Oyinade2020 <104277141+Oyinade2020@users.noreply.github.com>
2022-09-09 11:48:13 -0800

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
- * main- Entry point
+ * main - Entry point
  *
- * Return: Always 0 (success)
- *
- * Description: prints uppercase and lowercase letters
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-	for (c = 'A'; c <= 'Z'; c++);
-		putchar(c);
+		putchar(love);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(love);
 	putchar('\n');
 	return (0);
+}
