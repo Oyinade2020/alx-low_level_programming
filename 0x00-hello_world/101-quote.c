34c1dc83bdfeb5c9ce89abbd7f7fd0706c747ac6
Oyinlola Adesokan <sockistical@gmail.com>
2022-09-08 17:56:23 -0700

    last but not the least

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
  *
  * Return : Akways 0 (success)
  */
-
 int main(void)
 {
 	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-       return (0);
+       return (1);
 }       
