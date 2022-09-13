d0145b9992b8cfd70d4cde87e88b7058cebdb91a
Oyinlola Adesokan <sockistical@gmail.com>
2022-09-13 15:56:42 -0700

    function prints out alphabet in lowercase thricel

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,11 @@
  * Return: 0
  *
  * Description: "print_alphabet_x10.c: function to print abc 10 times"
- *
  */
 void print_alphabet_x10(void)
 {
 	char c, i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
