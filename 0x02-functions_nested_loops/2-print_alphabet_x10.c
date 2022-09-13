766d03835eee0e320f0f3a857b94a216243c3fe5
Oyinlola Adesokan <sockistical@gmail.com>
2022-09-13 15:42:25 -0700

    function prints out alphabet in lowercase thrice

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main- Entry point
+ *
+ * Return: 0
+ *
+ * Description: print_alphabet_x10.c: function to print abc 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
