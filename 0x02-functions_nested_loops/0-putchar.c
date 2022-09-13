5b1a8a29c05b51404bb0d5b8b44b113b1f99249e
Oyinlola Adesokan <sockistical@gmail.com>
2022-09-13 14:54:22 -0700

    Printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main- prints out _putchar
  *
@@ -7,7 +7,16 @@
 
 int main(void)
 {
-	printf('_putchar');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
