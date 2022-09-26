28860a7d7928899a8b7f4e22d06a3c9de555c14f
Oyinlola Adesokan <sockistical@gmail.com>
2022-09-26 12:52:29 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,7 +17,7 @@ char *_strstr(char *haystack, char *needle)
 		while (*haystack == *b && *b != 0 && *haystack != 0)
 			haystack++, b++;
 		if (*b == 0)
-			return(a);
+			return (a);
 		haystack = ++a;
 	}
 	return (0);
