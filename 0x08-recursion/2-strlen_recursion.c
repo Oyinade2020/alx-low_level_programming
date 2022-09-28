e6aea68dadc17f7b1080ecc9a04fb8bfa1349f42
Oyinlola Adesokan <sockistical@gmail.com>
2022-09-28 00:33:29 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * strlen_recursion - returns the length of a string
+ *
+ * @s: pointer to char to check
+ *
+ * Return: 0 is success
+ */
+int strlen_recursion(char *s)
+{
+	int c;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		c = _strlen_recursion(s + 1);
+	}
+	return (c + 1);
+}
