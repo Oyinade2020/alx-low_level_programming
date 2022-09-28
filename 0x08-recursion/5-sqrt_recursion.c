fd7caf43554731f0293dc75d3ec93ad55376c52f
Oyinlola Adesokan <sockistical@gmail.com>
2022-09-28 09:33:16 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,27 @@
 #include "main.h"
 /**
- * _alcuadrado - gives the square root of a number
- *
- * @n: given number
- * @corre: from 0 up looking for square always less than half the number
- * Return: if it does not have square or corre it finds it
- */
-int _alcuadrado(int n, int corre)
-{
-	if (corre < 0)
-		return (-1);
-	if (corre == 1)
-		return (1);
-	if (n > corre / 2)
-		return (-1);
-	if (corre != n * n)
-		return (_alcuadrado(corre, n + 1));
-	if (corre == n * n)
-		return (n);
-	return (-1);
-}
-/**
- * _sqrt_recursion - returns the square root of a natural number
- *
- * @n: to check
- *
- * Return: the square root or -1 if there is not any
+ * _sqrt_recursion - find natural square root
+ * @n: int
+ * Return: int
  */
 int _sqrt_recursion(int n)
 {
-	return (_alcuadrado(n, 1));
+	return (square(n, 1));
+}
+
+/**
+ * square - find square root
+ * @n: int to find square root
+ * @val: square root
+ * Return: int
+ */
+
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return  (square(n, val + 1));
+	else
+		return (-1);
 }
