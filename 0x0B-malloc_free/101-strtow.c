39971c8c9fc14f3ef55ed6f9e720cd93b5a87308
Oyinlola Adesokan <sockistical@gmail.com>
2022-10-03 11:42:23 -0700

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,16 +7,16 @@
  *
  * Return: nothing
  */
-void print_tab(char **tab)
-{
-    int i;
-
-    for (i = 0; tab[i] != NULL; ++i)
-    {
-        printf("%s\n", tab[i]);
-    }
-}
-
+/*void print_tab(char **tab)
+*{
+*    int i;
+*
+*    for (i = 0; tab[i] != NULL; ++i)
+*    {
+*        printf("%s\n", tab[i]);
+*    }
+*}
+*/
 /**
  * main - check the code for ALX School students.
  *
