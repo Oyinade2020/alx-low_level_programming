b3966f64468636e061a1b5c2d6f0fb6a294e3e8b
Oyinlola Adesokan <sockistical@gmail.com>
2022-10-07 00:50:28 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,38 +1,30 @@
 #include <stdio.h>
+
 /**
- * main - program that prints out a number, buzz, fizz or fizzbuzz
- *
- * Return: returns the value 0
- */
+* main - fizz bizz holterton style
+* Description: Uses headers to link and a nested loops to achieve goal
+* fizz buzz from 1-100. print fizz for multiples of 3 and buzz for
+* multiples of 5 and fizzbuzz for multiples of both eg. 15
+* could use a switch for neater and better code
+* Return: 0 if no errors
+*/
+
 int main(void)
 {
-	int num = 0;
+	int i;
 
-	while (num++ < 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((num % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((num % 5) == 0)
-		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("Buzz ");
-			}
-		}
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
-		{
-			printf("%d ", num);
-		}
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
